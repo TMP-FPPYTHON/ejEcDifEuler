452743fada5803ba0cf07b3ec123b49c870acc50
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2021-08-18 22:27:21 +0000

    Resuelto el Metodo de Euler Cauchy

--- assignments/00Euler/src/exercise.py
+++ assignments/00Euler/src/exercise.py
@@ -1,7 +1,29 @@
+
+import os
+
+# Imprime valores tabulares del Método de Euler
+def printTablaEuler(x,y):
+    print(f"{'x':>8}{'y':>8}")
+    print(f"{'----------------':16}")
+    for i in range(0,len(x)):
+        print(f"{x[i]:8.2f}{y[i]:8.2f}")
+
+# Método de Euler
+def Euler(f,x,y, ls, h):
+    i=1
+    while i * h <= ls:
+        x = x + [(x[i-1]+h)]
+        y = y + [(y[i-1] + f(x[i-1],y[i-1])*h)]
+
+        i=i+1
+    
+    return x,y
+
 def main():
     #escribe tu código abajo de esta línea
+
+    os.system("clear")
     print("METODO DE EULER-CAUCHY")
-    print("======================")
 
     # Ecuacion Diferencial
     f = lambda x,y : -2 * x ** 3 + 12 * x ** 2 - 20 * x + 8.5
@@ -13,18 +35,11 @@ def main():
     ls=4
     h=0.5
 
-    i=1
-    while i * h <= ls:
-        x = x + [(x[i-1]+h)]
-        y = y + [(y[i-1] + f(x[i-1],y[i-1])*h)]
+    # Resolviendo la Eucacion Diferencial por el Método de Euler
+    x,y = Euler(f,x,y,ls,h)
 
-        i=i+1
-
-    print("Tabla Método de Euler")
-    print("=====================")
-
-    print(x)
-    print(y)
+    # Mostrando la solución tabular
+    printTablaEuler(x,y)
 
 if __name__=='__main__':
     main()
